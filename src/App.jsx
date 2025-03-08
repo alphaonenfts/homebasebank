7ad311b172bc74e665f53ccd86663ee8cd625c18
james.dev <alphaonenft4@gmail.com>
2025-03-08 10:16:31 +0100

    New Commi

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Form1 from './components/Form1'
 import Table from './components/Table'
+import Form2 from './components/Form2'
 
 
 const App = () => {
   return (
     <div>
-      <Form1/>
+      {/* <Form1/> */}
       {/* <Table /> */}
+      <Form2/>
     </div>
 
   )
